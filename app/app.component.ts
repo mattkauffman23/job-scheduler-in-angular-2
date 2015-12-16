122c4d153f6749af097207790052d1a199c46703
Matt Kauffman <mattkauffman@rocketmail.com>
2015-12-15 17:16:12 -0800

    First pass at job data model

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
 import {Component} from 'angular2/core';
 
+enum Size { Free, Small, Medium, Big, Giant };
+enum Frequency { Daily, Hourly, TenMinutes };
+
+interface Job {
+  id: number;
+  cmd: string;
+  size: Size;
+  frequency: Frequency;
+  nextDue: number;
+  lastRun: number;
+}
+
 @Component({
     selector: 'my-app',
     template: '<h1>my app title</h1>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+
+}
